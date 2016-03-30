dfa6771539bee53bcd85cd71b94c10568df32afd
pbalachandra <balachandra.p@gmail.com>
2016-03-31 00:41:50 +0530

    ACD_AN_Session_2_Assignment_4_Main
    ACD_AN_Session_2_Assignment_4_Main

---
+++ app/src/main/java/com/acadgild/balu/acd_an_session_2_assignment_4_main/MainActivity.java
@@ -0,0 +1,19 @@
+package com.acadgild.balu.acd_an_session_2_assignment_4_main;
+
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class MainActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
